f278abd55df393df762722a6d2e5b36bd5850ed9
Junior V. Ramisch <junior.ramisch@gmail.com>
2021-03-01 00:42:01 -0300

    feat(data): adds general info json

---
+++ src/data/general-info.js
@@ -0,0 +1,16 @@
+export default {
+    description: "Acadêmico de Ciência da Computação pela UFFS Chapecó. \
+    No tempo livre, e não tão livre assim, assiste anime, vê memes, joga \
+    Minecraft, ouve Nerdcast, rock/metal e eventualmente alguns J-POPs. \
+    Sua comida preferida é CupNoodles e biluzitos de churrasco. Gosta de \
+    contribuir com projetos open-source, seu grande sonho é fazer um cosplay \
+    de Naruto.",
+    twitter: "https://twitter.com/jr_ramisch",
+    github: "https://github.com/arufonsekun",
+    spotify: "https://open.spotify.com/user/21fon2iumnknv6vsvqscs56wy?si=7drQtrQwTQ2jMzfd9SThtA",
+    instagram: "https://instagram.com/jr_ramisch",
+    linkedin: "https://www.linkedin.com/in/junior-vitor-ramisch-062582145/",
+    telegram: "https://t.me/arufonsekun",
+    email: "mailto:junior.ramisch@gmail.com",
+    discord: "https://discord.com/login"
+}
