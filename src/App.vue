cc9178a1117516b9ed9826c801a81c1bf8bf61b5
Arufonsekun <junior.ramisch@gmail.com>
2022-07-29 04:29:30 -0300

    feat: Adiciona versão anterior na página

--- src/App.vue
+++ src/App.vue
@@ -1,91 +1,61 @@
 <script setup>
-import { RouterLink, RouterView } from "vue-router";
-import HelloWorld from "./components/HelloWorld.vue";
+    import { RouterLink, RouterView } from "vue-router";
+    import Sidebar from "@/components/Sidebar.vue";
 </script>
 
 <template>
-  <header>
-    <img
-      alt="Vue logo"
-      class="logo"
-      src="@/assets/logo.svg"
-      width="125"
-      height="125"
-    />
-
-    <div class="wrapper">
-      <HelloWorld msg="You did it!" />
-
-      <nav>
-        <RouterLink to="/">Home</RouterLink>
-        <RouterLink to="/about">About</RouterLink>
-      </nav>
+    <div id="app">
+        <div class="flex flex-row p-2">
+            <Sidebar />
+            <div class="
+                p-1
+                flex
+                justify-center
+                working-in-progress 
+            ">
+            <h1>
+                Work in progress...
+            </h1>
+            </div>
+        </div>
     </div>
-  </header>
-
-  <RouterView />
 </template>
 
 <style scoped>
-header {
-  line-height: 1.5;
-  max-height: 100vh;
+#app {
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  background-color: #eee ;
+}
+.working-in-progress {
+  align-items: center;
+  font-size: 4rem;
+  height: 97vh;
+  width: 75%;
+  animation: color-change 5s infinite;
 }
 
-.logo {
-  display: block;
-  margin: 0 auto 2rem;
-}
-
-nav {
-  width: 100%;
-  font-size: 12px;
-  text-align: center;
-  margin-top: 2rem;
-}
-
-nav a.router-link-exact-active {
-  color: var(--color-text);
-}
-
-nav a.router-link-exact-active:hover {
-  background-color: transparent;
-}
-
-nav a {
-  display: inline-block;
-  padding: 0 1rem;
-  border-left: 1px solid var(--color-border);
-}
-
-nav a:first-of-type {
-  border: 0;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  .logo {
-    margin: 0 2rem 0 0;
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-
-  nav {
-    text-align: left;
-    margin-left: -1rem;
-    font-size: 1rem;
-
-    padding: 1rem 0;
-    margin-top: 1rem;
-  }
+@keyframes color-change {
+  5%{color: #2C2549;}
+  10%{color: #27264B;}
+  15%{color: #282E4C;}
+  20%{color: #2A364E;}
+  25%{color: #2c3e50;}
+  30%{color: #455869;}
+  35%{color: #5F7282;}
+  40%{color: #798C9A;}
+  45%{color: #94A5B1;}
+  50%{color: #B0BEC7;}
+  55%{color: #B0BEC7;}
+  60%{color: #94A5B1;}
+  65%{color: #798C9A;}
+  70%{color: #5F7282;}
+  75%{color: #455869;}
+  80%{color: #2c3e50;}
+  85%{color: #2A364E;}
+  90%{color: #282E4C;}
+  95%{color: #27264B;}
+  100%{color: #2C2549;}
 }
 </style>
---
+++ src/components/Sidebar.vue
@@ -0,0 +1,99 @@
+<template>
+    <div class="flex flex-col sidebar shadow-xl rounded p-4 border-1 border-neutral-100">
+        <div>
+            <img
+                src="@/assets/images/me.jpeg"
+                alt="Arufonsekun"
+                class="rounded-full w-8/12 mx-auto brightness-150 hue-rotate-15"
+            />
+        </div>
+
+        <div class="name font-bold text-3xl text-left mx-4 my-3">
+            <h4>
+                Junior Vitor Ramisch
+            </h4>
+        </div>
+
+        <div class="mx-4">
+            <p class="text-base">
+                {{ description }}
+            </p>
+        </div>
+
+        <div class="flex mt-auto">
+            <div class="media">
+                <a :href="telegram" target="_blank" rel="noopener noreferrer">
+                    <img src="@/assets/images/telegram.svg" alt="Telegram" width="76">
+                </a>
+            </div>
+            <div class="media">
+                <a :href="twitter" target="_blank" rel="noopener noreferrer">
+                    <img src="@/assets/images/twitter.svg" alt="Twitter" width="76">
+                </a>
+            </div>
+            <div class="media">
+                <a :href="instagram" target="_blank" rel="noopener noreferrer">
+                    <img src="@/assets/images/instagram.svg" alt="Instagram" width="76">
+                </a>
+            </div>
+            <div class="media">
+                <a :href="linkedin" target="_blank" rel="noopener noreferrer">
+                    <img src="@/assets/images/linkedin.svg" alt="LinkedIn" width="76">
+                </a>
+            </div>
+            <div class="media">
+                <a :href="discord" target="_blank" rel="noopener noreferrer">
+                    <img src="@/assets/images/discord.svg" alt="Discord" width="76">
+                </a>
+            </div>
+            <div class="media">
+                <a :href="spotify" target="_blank" rel="noopener noreferrer">
+                    <img src="@/assets/images/spotify.svg" alt="Spotify" width="76">
+                </a>
+            </div>
+            <div class="media">
+                <a :href="github" target="_blank" rel="noopener noreferrer">
+                    <img src="@/assets/images/github.svg" alt="Github" width="76">
+                </a>
+            </div>
+            <div class="media">
+                <a :href="email" target="_blank" rel="noopener noreferrer">
+                    <img src="@/assets/images/email.svg" alt="Email" width="76">
+                </a>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+
+import GeneralInfo from "@/data/general-info";
+
+export default {
+    data(){
+        return {
+            description: GeneralInfo.description,
+            twitter: GeneralInfo.twitter,
+            github: GeneralInfo.github,
+            spotify: GeneralInfo.spotify,
+            instagram: GeneralInfo.instagram,
+            linkedin: GeneralInfo.linkedin,
+            telegram: GeneralInfo.telegram,
+            email: GeneralInfo.email,
+            discord: GeneralInfo.discord
+        }
+    },
+}
+</script>
+
+<style scoped>
+    .sidebar {
+        height: 97vh;
+        width: 25%;
+    }
+
+    .media img {
+        padding: 15%;
+    }
+
+</style>
