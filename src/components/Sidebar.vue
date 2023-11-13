c868ac6e257af6398ff2534e0dd2277486e13387
arufonsekun <junior.ramisch@gmail.com>
2023-11-13 20:49:27 -0300

    chore: Improves arufonsekun description

--- src/components/Sidebar.vue
+++ src/components/Sidebar.vue
@@ -1,99 +1,112 @@
 <template>
-    <div class="flex flex-col sidebar shadow-xl rounded p-4 border-1 border-neutral-100">
-        <div>
-            <img
-                src="@/assets/images/me.jpeg"
-                alt="Arufonsekun"
-                class="rounded-full w-8/12 mx-auto brightness-150 hue-rotate-15"
-            />
-        </div>
-
-        <div class="name font-bold text-3xl text-left mx-4 my-3">
-            <h4>
-                Junior Vitor Ramisch
-            </h4>
-        </div>
-
-        <div class="mx-4">
-            <p class="text-base">
-                {{ description }}
-            </p>
-        </div>
-
-        <div class="flex mt-auto">
-            <div class="media">
-                <a :href="telegram" target="_blank" rel="noopener noreferrer">
-                    <img src="@/assets/images/telegram.svg" alt="Telegram" width="76">
-                </a>
-            </div>
-            <div class="media">
-                <a :href="twitter" target="_blank" rel="noopener noreferrer">
-                    <img src="@/assets/images/twitter.svg" alt="Twitter" width="76">
-                </a>
-            </div>
-            <div class="media">
-                <a :href="instagram" target="_blank" rel="noopener noreferrer">
-                    <img src="@/assets/images/instagram.svg" alt="Instagram" width="76">
-                </a>
-            </div>
-            <div class="media">
-                <a :href="linkedin" target="_blank" rel="noopener noreferrer">
-                    <img src="@/assets/images/linkedin.svg" alt="LinkedIn" width="76">
-                </a>
-            </div>
-            <div class="media">
-                <a :href="discord" target="_blank" rel="noopener noreferrer">
-                    <img src="@/assets/images/discord.svg" alt="Discord" width="76">
-                </a>
-            </div>
-            <div class="media">
-                <a :href="spotify" target="_blank" rel="noopener noreferrer">
-                    <img src="@/assets/images/spotify.svg" alt="Spotify" width="76">
-                </a>
-            </div>
-            <div class="media">
-                <a :href="github" target="_blank" rel="noopener noreferrer">
-                    <img src="@/assets/images/github.svg" alt="Github" width="76">
-                </a>
-            </div>
-            <div class="media">
-                <a :href="email" target="_blank" rel="noopener noreferrer">
-                    <img src="@/assets/images/email.svg" alt="Email" width="76">
-                </a>
-            </div>
-        </div>
+  <div
+    class="flex flex-col sidebar shadow-xl rounded p-4 border-1 border-neutral-100"
+  >
+    <div>
+      <img
+        src="@/assets/images/me.jpeg"
+        alt="Arufonsekun"
+        class="rounded-full w-8/12 mx-auto"
+      />
     </div>
+
+    <div class="name font-bold text-3xl text-left mx-4 my-3">
+      <h4>Junior Vitor Ramisch</h4>
+    </div>
+
+    <div class="mx-4">
+      <p class="text-base">
+        Estudante de Ciência da Computação pela UFFS Chapecó e Dev Fullstack na
+        <a
+          href="https://www.optidata.cloud/"
+          class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+          target="_blank"
+        >
+          Optidata</a
+        >. Gosto de Daft Punk, The Office,
+        <a
+          href="https://www.op.gg/summoners/br/arufonsekun"
+          target="_blank"
+          rel="noopener noreferrer"
+          class="font-medium text-blue-600 dark:text-blue-500 hover:underline"
+          >LOL</a
+        >
+        e dos meus amigos da firma.
+      </p>
+    </div>
+
+    <div class="flex mt-auto">
+      <div class="media">
+        <a :href="telegram" target="_blank" rel="noopener noreferrer">
+          <img src="@/assets/images/telegram.svg" alt="Telegram" width="76" />
+        </a>
+      </div>
+      <div class="media">
+        <a :href="twitter" target="_blank" rel="noopener noreferrer">
+          <img src="@/assets/images/twitter.svg" alt="Twitter" width="76" />
+        </a>
+      </div>
+      <div class="media">
+        <a :href="instagram" target="_blank" rel="noopener noreferrer">
+          <img src="@/assets/images/instagram.svg" alt="Instagram" width="76" />
+        </a>
+      </div>
+      <div class="media">
+        <a :href="linkedin" target="_blank" rel="noopener noreferrer">
+          <img src="@/assets/images/linkedin.svg" alt="LinkedIn" width="76" />
+        </a>
+      </div>
+      <div class="media">
+        <a :href="discord" target="_blank" rel="noopener noreferrer">
+          <img src="@/assets/images/discord.svg" alt="Discord" width="76" />
+        </a>
+      </div>
+      <div class="media">
+        <a :href="spotify" target="_blank" rel="noopener noreferrer">
+          <img src="@/assets/images/spotify.svg" alt="Spotify" width="76" />
+        </a>
+      </div>
+      <div class="media">
+        <a :href="github" target="_blank" rel="noopener noreferrer">
+          <img src="@/assets/images/github.svg" alt="Github" width="76" />
+        </a>
+      </div>
+      <div class="media">
+        <a :href="email" target="_blank" rel="noopener noreferrer">
+          <img src="@/assets/images/email.svg" alt="Email" width="76" />
+        </a>
+      </div>
+    </div>
+  </div>
 </template>
 
 <script>
-
 import GeneralInfo from "@/data/general-info";
 
 export default {
-    data(){
-        return {
-            description: GeneralInfo.description,
-            twitter: GeneralInfo.twitter,
-            github: GeneralInfo.github,
-            spotify: GeneralInfo.spotify,
-            instagram: GeneralInfo.instagram,
-            linkedin: GeneralInfo.linkedin,
-            telegram: GeneralInfo.telegram,
-            email: GeneralInfo.email,
-            discord: GeneralInfo.discord
-        }
-    },
-}
+  data() {
+    return {
+      description: GeneralInfo.description,
+      twitter: GeneralInfo.twitter,
+      github: GeneralInfo.github,
+      spotify: GeneralInfo.spotify,
+      instagram: GeneralInfo.instagram,
+      linkedin: GeneralInfo.linkedin,
+      telegram: GeneralInfo.telegram,
+      email: GeneralInfo.email,
+      discord: GeneralInfo.discord,
+    };
+  },
+};
 </script>
 
 <style scoped>
-    .sidebar {
-        height: 97vh;
-        width: 25%;
-    }
+.sidebar {
+  height: 97vh;
+  width: 25%;
+}
 
-    .media img {
-        padding: 15%;
-    }
-
+.media img {
+  padding: 15%;
+}
 </style>
--- src/data/general-info.js
+++ src/data/general-info.js
@@ -1,15 +1,11 @@
 export default {
-    description: "Acadêmico de Ciência da Computação pela UFFS Chapecó. \
-    Assiste animes, vê memes, joga Minecraft e League of Legends, ouve Nerdcast,\
-    rock/metal e eventualmente alguns J-POPs. Sua comida preferida é CupNoodles \
-    e biluzitos de churrasco. Gosta de contribuir com projetos open-source, seu \
-    grande sonho é fazer um cosplay de Naruto.",
-    twitter: "https://twitter.com/jr_ramisch",
-    github: "https://github.com/arufonsekun",
-    spotify: "https://open.spotify.com/user/21fon2iumnknv6vsvqscs56wy?si=7drQtrQwTQ2jMzfd9SThtA",
-    instagram: "https://instagram.com/jr_ramisch",
-    linkedin: "https://www.linkedin.com/in/junior-vitor-ramisch-062582145/",
-    telegram: "https://t.me/arufonsekun",
-    email: "mailto:junior.ramisch@gmail.com",
-    discord: "https://discord.com/login"
-}
+  twitter: "https://twitter.com/jr_ramisch",
+  github: "https://github.com/arufonsekun",
+  spotify:
+    "https://open.spotify.com/user/21fon2iumnknv6vsvqscs56wy?si=7drQtrQwTQ2jMzfd9SThtA",
+  instagram: "https://instagram.com/jr_ramisch",
+  linkedin: "https://www.linkedin.com/in/junior-vitor-ramisch-062582145/",
+  telegram: "https://t.me/arufonsekun",
+  email: "mailto:junior.ramisch@gmail.com",
+  discord: "https://discord.com/login",
+};
