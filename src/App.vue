58a255d47b9f75050d5f3f025c61b3f841716850
arufonsekun <junior.ramisch@gmail.com>
2024-01-22 22:23:27 -0300

    feat: New version of landing page with bootstrap

--- src/App.vue
+++ src/App.vue
@@ -1,24 +1,150 @@
 <script setup>
-    import { RouterLink, RouterView } from "vue-router";
-    import Sidebar from "@/components/Sidebar.vue";
+import GeneralInfo from "./data/general-info";
 </script>
 
 <template>
-    <div id="app">
-        <div class="flex flex-row p-2">
-            <Sidebar />
-            <div class="
-                p-1
-                flex
-                justify-center
-                working-in-progress 
-            ">
-            <h1>
-                Work in progress...
-            </h1>
+  <div class="container-fluid">
+    <div class="row user-select-none">
+      <div class="col-md-3 info shadow">
+        <div
+          class="d-flex flex-column justify-content-between"
+          style="height: inherit"
+        >
+          <div class="col-12">
+            <div class="d-flex justify-content-center py-5">
+              <img
+                src="@/assets/images/me.jpeg"
+                class="rounded-circle d-sm-block d-none border-3 border"
+                alt="Junior Vitor Ramisch"
+                width="250px"
+              />
+              <img
+                src="@/assets/images/me.jpeg"
+                class="rounded-circle d-sm-none border-3 border"
+                alt="Junior Vitor Ramisch"
+                width="200px"
+              />
             </div>
+            <div class="col-12">
+              <h3 class="text-center fw-bolder">Junior Vitor Ramisch</h3>
+              <p>
+                Estudante de Ciência da Computação pela UFFS Chapecó e Dev
+                Fullstack na
+                <a
+                  href="https://www.optidata.cloud/"
+                  class="fw-bolder"
+                  target="_blank"
+                >
+                  Optidata</a
+                >. Gosto de Daft Punk, The Office,
+                <a
+                  href="https://www.op.gg/summoners/br/arufonsekun"
+                  target="_blank"
+                  rel="noopener noreferrer"
+                  class="fw-bolder"
+                  >LOL</a
+                >
+                e dos meus amigos da firma.
+              </p>
+            </div>
+          </div>
+          <div class="col-12 d-flex justify-content-around p-sm-3 p-2">
+            <div class="media">
+              <a
+                :href="GeneralInfo.telegram"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="@/assets/images/telegram.svg"
+                  alt="Telegram"
+                  class="p-sm-25 p-1"
+                />
+              </a>
+            </div>
+            <div class="media">
+              <a
+                :href="GeneralInfo.twitter"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="@/assets/images/twitter.svg"
+                  alt="Twitter"
+                  class="p-sm-25 p-1"
+                />
+              </a>
+            </div>
+            <div class="media">
+              <a
+                :href="GeneralInfo.instagram"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="@/assets/images/instagram.svg"
+                  alt="Instagram"
+                  class="p-sm-25 p-1"
+                />
+              </a>
+            </div>
+            <div class="media">
+              <a
+                :href="GeneralInfo.linkedin"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="@/assets/images/linkedin.svg"
+                  alt="LinkedIn"
+                  class="p-sm-25 p-1"
+                />
+              </a>
+            </div>
+            <div class="media">
+              <a
+                :href="GeneralInfo.spotify"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="@/assets/images/spotify.svg"
+                  alt="Spotify"
+                  class="p-sm-25 p-1"
+                />
+              </a>
+            </div>
+            <div class="media">
+              <a
+                :href="GeneralInfo.github"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="@/assets/images/github.svg"
+                  alt="Github"
+                  class="p-sm-25 p-1"
+                />
+              </a>
+            </div>
+            <div class="media">
+              <a
+                :href="GeneralInfo.email"
+                target="_blank"
+                rel="noopener noreferrer"
+              >
+                <img
+                  src="@/assets/images/email.svg"
+                  alt="Email"
+                  class="p-sm-25 p-1"
+                />
+              </a>
+            </div>
+          </div>
         </div>
+      </div>
     </div>
+  </div>
 </template>
 
 <style scoped>
@@ -26,7 +152,7 @@
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   color: #2c3e50;
-  background-color: #eee ;
+  background-color: #eee;
 }
 .working-in-progress {
   align-items: center;
@@ -37,25 +163,79 @@
 }
 
 @keyframes color-change {
-  5%{color: #2C2549;}
-  10%{color: #27264B;}
-  15%{color: #282E4C;}
-  20%{color: #2A364E;}
-  25%{color: #2c3e50;}
-  30%{color: #455869;}
-  35%{color: #5F7282;}
-  40%{color: #798C9A;}
-  45%{color: #94A5B1;}
-  50%{color: #B0BEC7;}
-  55%{color: #B0BEC7;}
-  60%{color: #94A5B1;}
-  65%{color: #798C9A;}
-  70%{color: #5F7282;}
-  75%{color: #455869;}
-  80%{color: #2c3e50;}
-  85%{color: #2A364E;}
-  90%{color: #282E4C;}
-  95%{color: #27264B;}
-  100%{color: #2C2549;}
+  5% {
+    color: #2c2549;
+  }
+  10% {
+    color: #27264b;
+  }
+  15% {
+    color: #282e4c;
+  }
+  20% {
+    color: #2a364e;
+  }
+  25% {
+    color: #2c3e50;
+  }
+  30% {
+    color: #455869;
+  }
+  35% {
+    color: #5f7282;
+  }
+  40% {
+    color: #798c9a;
+  }
+  45% {
+    color: #94a5b1;
+  }
+  50% {
+    color: #b0bec7;
+  }
+  55% {
+    color: #b0bec7;
+  }
+  60% {
+    color: #94a5b1;
+  }
+  65% {
+    color: #798c9a;
+  }
+  70% {
+    color: #5f7282;
+  }
+  75% {
+    color: #455869;
+  }
+  80% {
+    color: #2c3e50;
+  }
+  85% {
+    color: #2a364e;
+  }
+  90% {
+    color: #282e4c;
+  }
+  95% {
+    color: #27264b;
+  }
+  100% {
+    color: #2c2549;
+  }
+}
+
+.info {
+  height: 100vh;
+}
+
+.media {
+  img {
+    width: 45px;
+  }
+}
+
+img {
+  -webkit-user-drag: none;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -5,6 +5,20 @@ import App from "./App.vue";
 import router from "./router";
 
 import "./assets/main.css";
+import {
+  Alert,
+  Button,
+  Carousel,
+  Collapse,
+  Dropdown,
+  Modal,
+  Offcanvas,
+  Popover,
+  ScrollSpy,
+  Tab,
+  Toast,
+  Tooltip,
+} from "bootstrap";
 
 const app = createApp(App);
 
@@ -12,3 +26,17 @@ app.use(createPinia());
 app.use(router);
 
 app.mount("#app");
+window.bootstrap = {
+  Alert,
+  Button,
+  Carousel,
+  Collapse,
+  Dropdown,
+  Modal,
+  Offcanvas,
+  Popover,
+  ScrollSpy,
+  Tab,
+  Toast,
+  Tooltip,
+};
